679abbcb874569c2dee5b0b363a7ec8e2609697a
ollielynas <lynasollie@gmail.com>
2023-06-30 09:43:33 +1200

    new build

--- dist/password-game-clone-627f128319692cbd.js
+++ dist/password-game-clone-627f128319692cbd.js
@@ -596,7 +596,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('password-game-clone-23bd9931f3954f4f_bg.wasm', import.meta.url);
+        input = new URL('password-game-clone-627f128319692cbd_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
