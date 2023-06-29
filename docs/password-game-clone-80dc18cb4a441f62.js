0658fe3d3bbeffc9bfb4f68e2f552910d444f21f
ollielynas <lynasollie@gmail.com>
2023-06-30 01:01:10 +1200

    Update password-game-clone-80dc18cb4a441f62.js

--- docs/password-game-clone-80dc18cb4a441f62.js
+++ docs/password-game-clone-80dc18cb4a441f62.js
@@ -596,7 +596,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('password-game-clone-80dc18cb4a441f62_bg.wasm', import.meta.url);
+        input = new URL('docs/password-game-clone-80dc18cb4a441f62_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
